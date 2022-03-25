57d6a3fadf978e6a89c26904c21429f7c9b27df4
Afsaruddin12133 <afsaruddin12133@gmail.com>
2022-03-26 05:57:08 +0600

    added item remove

--- src/Component/Addorder/Addorder.js
+++ src/Component/Addorder/Addorder.js
@@ -1,9 +1,10 @@
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import './Addorder.css';
 import Item from './SelectItem/item';
 const Addorder = (props) => {
+    const [state , setState] = useState(0)
    if(props.watchinfo.length > 4){
        alert('You added more then Four Watch');
    }
@@ -12,7 +13,6 @@ const Addorder = (props) => {
             <h2 id='headdin'>Selected Watch</h2>
               {
                 (props.watchinfo.length > 4)? props.watchinfo.slice(0,4).map(data => <Item data = {data} key = {data.id}></Item>) :props.watchinfo.map(data => <Item data = {data} key = {data.id}></Item>)
-                    
               }
                 <button type='button'id='btn'>Choose Best For Me  
                 </button> <br />
--- src/Component/Addorder/SelectItem/item.js
+++ src/Component/Addorder/SelectItem/item.js
@@ -4,7 +4,7 @@ import './item.css';
 const Item = (props) => {
     const {img,name} = props.data
     return (
-        <div className='iconroot'>
+        <div id='iconroot'>
            <img src= {img} alt="" id='icon'/>
            <h3 id='name'>{name}</h3>
         </div>
