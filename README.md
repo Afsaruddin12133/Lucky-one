0ba2096e05b8027488c1bb9f15a9194448fcc05c
Afsaruddin12133 <afsaruddin12133@gmail.com>
2022-03-26 14:53:00 +0600

    update readme file

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# This is Watchshop
+# This is Luxury time app
+## Live site
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+ Luxury time app Live site link :[](https://facebook.github.io/create-react-app/docs/getting-started).
 
 ## Available Scripts
 
--- src/Component/Addorder/Addorder.js
+++ src/Component/Addorder/Addorder.js
@@ -1,6 +1,6 @@
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import './Addorder.css';
 import Item from './SelectItem/item';
 
@@ -12,16 +12,22 @@ const Addorder = (props) => {
    const removeallitem = () =>{
      document.getElementById('rootitem').textContent = " ";
    }
-
+   const [state ,setState] = useState(0)
    const chooseBest = () =>{
-    console.log("hello Afsar")
+    document.getElementById('rootitem').textContent = " ";
+    let index = Math.round(Math.random() * props.watchinfo.length)
+    setState(index)
   }
+  console.log(state)
     return (
         <div className='addorder'>
             <h2 id='headdin'>Selected Watch</h2>
              <div id='rootitem'>
              {
                 (props.watchinfo.length > 4)? props.watchinfo.slice(0,4).map(data => <Item data = {data} key = {data.id}></Item>) :props.watchinfo.map(data => <Item data = {data} key = {data.id}></Item>)
+              }
+              {
+                (props.watchinfo)? props.watchinfo.slice(0 , state).map(data => <Item data = {data} key = {data.id}></Item>) : console.log('Hello')
               }
               
              </div>
